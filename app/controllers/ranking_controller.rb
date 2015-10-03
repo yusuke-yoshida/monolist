a0dd271def1fb0ee7b1f87ecbeda2bf0b050fbdc
yusuke_yoshida <yoshidy37@gmail.com>
2015-10-03 16:11:30 +0000

    fixed ranking controller

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,11 +5,11 @@ class RankingController < ApplicationController
     
     def want
         @item_id = Want.group(:item_id).order("count_item_id desc").limit(10).count(:item_id).keys
-        @items = Item.find(@item_id)
+        @items = Item.find(@item_id).sort_by {|item| @item_id.index(item.id) }
     end
     
     def have
         @item_id = Have.group(:item_id).order("count_item_id desc").limit(10).count(:item_id).keys
-        @items = Item.find(@item_id)
+        @items = Item.find(@item_id).sort_by {|item| @item_id.index(item.id) }
     end
 end
